30d4a694a0eab81db0f40f956fe44860eb0f384d
NishkalHundia <nishkalnhundia@gmail.com>
2024-03-30 03:54:04 -0400

    added basic text to speech.

--- backend.py
+++ backend.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, jsonify
 import requests
 from faster_whisper import WhisperModel
+import torch
+from TTS.api import TTS
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
 
 model_size = "medium"
 model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
@@ -29,7 +35,12 @@ def process_audio():
     for segment in segments:
         final_text += segment.text
 
+    #ping gemini for the answer
+    sample_text = "This is a sample output from gemini. I am seeing how good the TTS is at this job."
+
+    tts.tts_to_file(sample_text, speaker_wav = "target/the_wolf_of_wall_street_speech-cut.wav", language="en", file_path="sample_output.wav")
+
     return final_text
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = False)
