540dbd836ee38d1514e3de93b805518159de70c1
ad0rshcodes <adarsharma.me@gmail.com>
2024-03-30 17:50:35 -0400

    Add Login Page

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/UploadImagePage';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
+import LoginPage from './components/LoginPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-ftz5nmqob4imyhw6.us.auth0.com"
+      clientId="Oakn4DPSLUUr5BQVvYBlPJN46wskRo05"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+    <LoginPage />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
