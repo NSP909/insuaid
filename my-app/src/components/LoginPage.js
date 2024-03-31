ae6122834410194d010add0c5122c332fe8d9b7b
ad0rshcodes <adarsharma.me@gmail.com>
2024-03-30 20:14:32 -0400

    configure routing and Auth0

--- my-app/src/components/LoginPage.js
+++ my-app/src/components/LoginPage.js
@@ -1,10 +1,24 @@
-import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+import React, { useEffect } from 'react'; // Import React and any hooks you are using
+import { useAuth0 } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom';
+
 
 const LoginPage = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  const navigate = useNavigate();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  React.useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/upload');
+    }
+  }, [isAuthenticated, navigate]);
+
+  return (
+    <div>
+      {/* ... your login page contents ... */}
+      <button onClick={() => loginWithRedirect()}>Log In</button>
+    </div>
+  );
 };
 
 export default LoginPage;
