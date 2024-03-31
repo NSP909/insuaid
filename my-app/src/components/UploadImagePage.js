bd3c303b31af21a10a0b57245dc77b76a5d2a1b0
ad0rshcodes <adarsharma.me@gmail.com>
2024-03-30 22:58:00 -0400

    send email id on logging in

--- my-app/src/components/UploadImagePage.js
+++ my-app/src/components/UploadImagePage.js
@@ -3,9 +3,37 @@ import AudioRecorder from './audioRecorder';
 import InsuranceInfoForm from './insuranceRequest';
 import SplitScreen from './splitScreen';
 import { FaMoneyBillAlt } from 'react-icons/fa';
+import React, { useEffect } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 
 function UploadImagePage() {
+
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+
+  console.log(user.email)
+  useEffect(() => {
+
+    const sendEmail = async () => {
+      const accessToken = await getAccessTokenSilently();
+
+      await fetch('http://127.0.0.1:5000/updateemail', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({ email: user.email }),
+      });
+    };
+
+
+    if (isAuthenticated && user) {
+      sendEmail().catch(console.error);
+    }
+  }, [isAuthenticated, user, getAccessTokenSilently]);
+
   return (
     <div className="App">
       <header className="App-header">
